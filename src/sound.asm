32894fef2e1b8f8e1aeb8119d92f622d21348f7d
seandolinar <sjjd199@gmail.com>
2020-06-16 23:20:35 -0400

    working sound effects!

--- src/sound.asm
+++ src/sound.asm
@@ -1,17 +1,29 @@
 .segment "CODE"
 soundCollision:
     ; need timer so that the sound will continue to play if we are sitting on it
-    LDA masterTimer
+    ; LDA masterTimer
     ; CMP #$00
     ; BNE dumpSoundCollision
     ; this makes it go on forever...don't know why
     ; because 5th bit (from right) disables the length counter
     ; LDA #%10111111;
     ; STA $4000
-    LDA #$ff    ;0C9 is a C# in NTSC mode
-    STA $4002
-    LDA #$02 ;LDA %11110000;
-    STA $4003
+
+    ; LDA #$ff    ;0C9 is a C# in NTSC mode
+    ; STA $4002
+    ; LDA #$02 ;LDA %11110000;
+    ; STA $4003
+
+
+    TXA
+    PHA
+    LDA #$00
+
+    ldx #FT_SFX_CH0
+	jsr FamiToneSfxPlay
+
+    PLA
+    TAX
 
 dumpSoundCollision:
     RTS
@@ -32,8 +44,16 @@ soundCollisionGood:
     ; LDA %11111111;
     ; STA $4003
 
-    ; ldx #FT_SFX_CH0
-	; jsr FamiToneSfxPlay
+
+    TXA
+    PHA
+    LDA #$00
+
+    ldx #FT_SFX_CH0
+	jsr FamiToneSfxPlay
+
+    PLA
+    TAX
 
 dumpSoundCollisionGood:
     RTS
@@ -41,7 +61,7 @@ dumpSoundCollisionGood:
 
 soundDot:
     ; need timer so that the sound will continue to play if we are sitting on it
-    LDA masterTimer
+    ; LDA masterTimer
     ; CMP #$00
     ; BNE dumpSoundCollision
     ; this makes it go on forever...don't know why
@@ -50,25 +70,31 @@ soundDot:
     ; STA $4000
    
    
-    LDA #$ff    ;0C9 is a C# in NTSC mode
-    STA $4002
-    LDA %11111111;
-    STA $4003
+    ; LDA #$ff    ;0C9 is a C# in NTSC mode
+    ; STA $4002
+    ; LDA %11111111;
+    ; STA $4003
 
-    ; ENABLE SOUND
-    lda #%00000000
-    sta $4015 ;enable square 1
+    ; ; ENABLE SOUND
+    ; lda #%00000000
+    ; sta $4015 ;enable square 1
 
-    lda #%00000001
-    sta $4015 ;enable square 1
+    ; lda #%00000001
+    ; sta $4015 ;enable square 1
 
-    LDA #$f0    ;0C9 is a C# in NTSC mode
-    STA $4002
-    LDA %11001100;
-    STA $4003
+    ; LDA #$f0    ;0C9 is a C# in NTSC mode
+    ; STA $4002
+    ; LDA %11001100;
+    ; STA $4003
 
-    ldx #FT_SFX_CH0
-	jsr FamiToneSfxPlay
+    TXA
+    PHA
+    LDA #$02
+
+    LDX #FT_SFX_CH0
+	JSR FamiToneSfxPlay
+    PLA
+    TAX
 
 dumpSoundDot:
     RTS
