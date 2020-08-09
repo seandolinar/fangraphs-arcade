6658cce56b764532fd9ae98e9dd4941d3641f969
seandolinar <sjjd199@gmail.com>
2020-08-09 15:23:01 -0400

    fixed eslint

--- web/src/InstructionBox.js
+++ web/src/InstructionBox.js
@@ -1,36 +1,44 @@
-import React from 'react';
+/* eslint-disable react/no-unescaped-entities */
+import React, { useState } from 'react';
 
 
 
-const InstructionBox = () => <div className="instruction-box">
+const InstructionBox = () => {
+
+    const [isOpen, setIsOpen] = useState(true);
+
+    return isOpen ? <div className="instruction-box" onClick={() => setIsOpen(false)}>
           Instructions
-          <ul>
+        <ul>
             <li>
               Click on TV's power button.
             </li>
             <li>
               Keyboard Controls
-              <ul>
-                <li>
+                <ul>
+                    <li>
                   Arrow keys are your direction pad or joystick.
-                </li>
-                <li>
+                    </li>
+                    <li>
                   The <strong>A Button</strong> corresponds to the <strong>X Key</strong> on the keyboard.
-                </li>
-                <li>
-                  You can pause using the <strong>Start Button</strong> or using the <strong>Enter Key</strong> on the keyboard.
-                </li>
-              </ul>
+                    </li>
+                    <li>
+                  You can pause using the 
+                        <strong>Start Button</strong> or using the 
+                        <strong>Enter Key</strong> on the keyboard.
+                    </li>
+                </ul>
             </li>
             <li>
               Game Play
-              <ul>
-                <li>You get three outs per inning.</li>
-                <li>Press the "A Button" or "X Key" to continue after an out.</li>
-                <li>Like baseball this game doesn't end.</li>
-              </ul>
+                <ul>
+                    <li>You get three outs per inning.</li>
+                    <li>Press the "A Button" or "X Key" to continue after an out.</li>
+                    <li>Like baseball this game doesn't end.</li>
+                </ul>
             </li>
-          </ul>
-        </div>;
+        </ul>
+    </div> : <div>closed</div> ;
+};
 
 export default InstructionBox;
