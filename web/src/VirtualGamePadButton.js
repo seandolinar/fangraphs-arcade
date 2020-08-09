5cf9c4962f8f94bfe602d36f61f57d27480a5a3f
seandolinar <sjjd199@gmail.com>
2020-08-09 19:54:10 -0400

    reverted mobile

--- web/src/VirtualGamePadButton.js
+++ web/src/VirtualGamePadButton.js
@@ -14,6 +14,7 @@ const VirtualGamePadButton = ({
 
     return <div className={`virtual-game-pad__button__wrapper ${className}`}>
         <div className="virtual-game-pad__button"
+            data-button={button}
             onMouseDown={() => onButtonDown(controller, button)}
             onMouseUp={() => onButtonUp(controller, button)}
             onTouchStart={() => onButtonDown(controller, button)}
@@ -24,7 +25,7 @@ const VirtualGamePadButton = ({
                 Controller.BUTTON_RIGHT,
                 Controller.BUTTON_UP,
                 Controller.BUTTON_DOWN
-            ], button) && <div className="button-dpad__arrow"></div>}
+            ], button) && <div className={`button-dpad__arrow d-${button}`}></div>}
         </div>
     </div>;
 };
